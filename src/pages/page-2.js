611dd4d49575ef409e6af30467127e79b5fda278
Stephen Karpeles <stephen@stephenkarpeles.com>
2021-04-05 11:14:37 -0500

    Update package.json

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,7 +1,7 @@
 import * as React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
+//import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { useTranslation } from "react-i18next"
 
