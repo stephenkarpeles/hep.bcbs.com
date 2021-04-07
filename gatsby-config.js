b7975ff4cc477cee6555b11c608a2051d2c0182d
John Cionci <john@thinkoomph.com>
2021-04-07 13:27:16 -0400

    update readme and site meta

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   pathPrefix: `/the-health-of-america/blue-health-equity`,
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Blue Health Equity`,
+    description: `Blue Cross and Blue Shield Companies’ National Health Equity Pledge.`,
+    author: `@oomphinc`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
