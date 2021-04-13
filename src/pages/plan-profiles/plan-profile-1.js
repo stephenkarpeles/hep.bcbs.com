e28efafad2e56ece91d5a6c74efc4ff62aadee73
Stephen Karpeles <stephen@stephenkarpeles.com>
2021-04-13 10:09:54 -0500

    Related fixes

--- src/pages/plan-profiles/plan-profile-1.js
+++ src/pages/plan-profiles/plan-profile-1.js
@@ -1,5 +1,5 @@
 import * as React from "react"
-//import { Link } from "gatsby"
+import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 import "../../components/pages.css"
 
@@ -46,7 +46,35 @@ const PlanProfile1 = () => {
       </div>
       
       <div className="pp-main">
-        <div className="pp-related"></div>
+        <div className="pp-related">
+          <h5 className="pp-related__title">Related</h5>
+        <ul className="pp-related__list">
+          <li className="pp-related__list-item">
+            <div className="pp-related__category">
+              <span>Health of America</span>
+            </div>
+            <div className="pp-related__text">
+              <Link to="/">Doulas of color can help moms of color have healthier babies</Link>
+            </div>
+          </li>
+          <li className="pp-related__list-item">
+            <div className="pp-related__category">
+              <span>Report</span>
+            </div>
+            <div className="pp-related__text">
+              <Link to="/">Inclusive and equitable perinatal care for pregnant Black women</Link>
+            </div>
+          </li>
+          <li className="pp-related__list-item">
+            <div className="pp-related__category">
+              <span>Report</span>
+            </div>
+            <div className="pp-related__text">
+              <Link to="/">In New Jersey, addressing postpartum depression in Black mothers</Link>
+            </div>
+          </li>
+        </ul>
+        </div>
         <div className="pp-content">
           <div className="pp-content__inner">
             <p>For every mother who dies a pregnancy- related death in the U.S., 70 experience a life threatening event. These are unexpected cases of what doctors call severe maternal morbidity (SMM) events such as cardiac arrest, hemorrhage or sepsis, events that could have been fatal or can affect a woman long after childbirth. The overall rate of these events has increased by over 200% in the past two decades. And the statistics are worse for Black, Indigenous and other women of color (BIPOC). Researchers at the University of Illinois Chicago found that Black women face a 70% higher risk of SMM than any other racial group.</p>
