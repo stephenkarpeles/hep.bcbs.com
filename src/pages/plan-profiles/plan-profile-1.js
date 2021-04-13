21ed56939d1e168b4bc659bb875239017e948177
Stephen Karpeles <stephen@stephenkarpeles.com>
2021-04-13 10:15:06 -0500

    Drop caps fix

--- src/pages/plan-profiles/plan-profile-1.js
+++ src/pages/plan-profiles/plan-profile-1.js
@@ -78,6 +78,7 @@ const PlanProfile1 = () => {
         <div className="pp-content">
           <div className="pp-content__inner">
             <p>For every mother who dies a pregnancy- related death in the U.S., 70 experience a life threatening event. These are unexpected cases of what doctors call severe maternal morbidity (SMM) events such as cardiac arrest, hemorrhage or sepsis, events that could have been fatal or can affect a woman long after childbirth. The overall rate of these events has increased by over 200% in the past two decades. And the statistics are worse for Black, Indigenous and other women of color (BIPOC). Researchers at the University of Illinois Chicago found that Black women face a 70% higher risk of SMM than any other racial group.</p>
+            <p>For every mother who dies a pregnancy- related death in the U.S., 70 experience a life threatening event. These are unexpected cases of what doctors call severe maternal morbidity (SMM) events such as cardiac arrest, hemorrhage or sepsis, events that could have been fatal or can affect a woman long after childbirth. The overall rate of these events has increased by over 200% in the past two decades. And the statistics are worse for Black, Indigenous and other women of color (BIPOC). Researchers at the University of Illinois Chicago found that Black women face a 70% higher risk of SMM than any other racial group.</p>
           </div>
         </div>
       </div>
