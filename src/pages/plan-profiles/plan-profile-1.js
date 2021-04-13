addb522232ee23f5c13c552814f5880b942d3113
Stephen Karpeles <stephen@stephenkarpeles.com>
2021-04-13 10:25:38 -0500

    blockquote content

--- src/pages/plan-profiles/plan-profile-1.js
+++ src/pages/plan-profiles/plan-profile-1.js
@@ -78,7 +78,13 @@ const PlanProfile1 = () => {
         <div className="pp-content">
           <div className="pp-content__inner">
             <p>For every mother who dies a pregnancy- related death in the U.S., 70 experience a life threatening event. These are unexpected cases of what doctors call severe maternal morbidity (SMM) events such as cardiac arrest, hemorrhage or sepsis, events that could have been fatal or can affect a woman long after childbirth. The overall rate of these events has increased by over 200% in the past two decades. And the statistics are worse for Black, Indigenous and other women of color (BIPOC). Researchers at the University of Illinois Chicago found that Black women face a 70% higher risk of SMM than any other racial group.</p>
-            <p>For every mother who dies a pregnancy- related death in the U.S., 70 experience a life threatening event. These are unexpected cases of what doctors call severe maternal morbidity (SMM) events such as cardiac arrest, hemorrhage or sepsis, events that could have been fatal or can affect a woman long after childbirth. The overall rate of these events has increased by over 200% in the past two decades. And the statistics are worse for Black, Indigenous and other women of color (BIPOC). Researchers at the University of Illinois Chicago found that Black women face a 70% higher risk of SMM than any other racial group.</p>
+            <h4>The role of doulas in reducing SMM</h4>
+            <p>Doulas provide emotional, physical, and informational support to women before, during and after childbirth. They’re trained but non-clinical partners to women, often supplementing care from doctors and midwives. There’s evidence to suggest that working with a doula can reduce SMM among BIPOC women. According to the March of Dimes, doula care can help reduce c-sections, decrease maternal anxiety and depression and help coordinate care and communication between BIPOC women and their other healthcare providers. According to the March of Dimes, “The role of doula care in reducing c-sections is important, because c-sections contribute to the risk of maternal morbidity and mortality in initial and subsequent pregnancies.”</p>
+            <blockquote>
+              The role of doula care in reducing c-sections is important, because c-sections contribute to the risk of maternal morbidity and mortality in initial and subsequent pregnancies.
+            </blockquote>
+            <h4>Blue Cross and Blue Shield companies are increasing support for doulas</h4>
+            <p>Many BCBS companies are including doula coverage in their members’ plans, making philanthropic contributions to doula organizations that focus on underserved communities and funding training programs to increase the number of BIPOC doulas.</p>
           </div>
         </div>
       </div>
