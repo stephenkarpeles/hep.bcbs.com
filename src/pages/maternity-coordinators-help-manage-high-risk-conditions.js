cdc56a42c3f039615af9a42e585d10c635cd2a19
John Cionci <john@thinkoomph.com>
2021-04-16 21:06:56 -0400

    most post entries

--- src/pages/maternity-coordinators-help-manage-high-risk-conditions.js
+++ src/pages/maternity-coordinators-help-manage-high-risk-conditions.js
@@ -1,7 +1,173 @@
-import React from "react"
+// Base Imports
+import * as React from "react"
+import { Link } from "gatsby"
+import Fade from "react-reveal/Fade"
+import { InstantSearch, InfiniteHits, Configure } from "react-instantsearch-dom"
+import algoliasearch from "algoliasearch/lite"
 
-function placeholder3() {
-  return <div>placeholder page</div>
+// Styles
+import "../components/pages.css"
+
+// Components
+import SEO from "../components/seo"
+
+// Sharing
+import Sharing from "../components/Sharing/sharing"
+
+const searchClient = algoliasearch(
+  "B604WWKJH0",
+  "156ba268a0517559cd6a89921ae9cb5f"
+)
+
+const planResult = ({ hit }) => {
+  const { headline, topic, plans, slug, teaser, img_url, read_time } = hit
+
+  return (
+    <div className="plan-result-card">
+      <Link to={`/${slug}/`}></Link>
+      <div className="plan-result-card-plan">
+        <div className="plan-result-card-image">
+          <img
+            src={`https://www.bcbs.com/sites/default/files/healthequity/images/${img_url}`}
+            alt=""
+          />
+        </div>
+        <div className="plan-result-card-company">{plans}</div>
+      </div>
+      <div className="plan-result-card-meta">
+        <div className="plan-result-card-category">{topic}</div>
+        <div className="plan-result-card-title">{headline}</div>
+        <div className="plan-result-card-excerpt">{teaser}</div>
+        <div className="plan-result-card-read">{read_time} min read</div>
+      </div>
+    </div>
+  )
 }
 
-export default placeholder3
+const MaternityCoordinators = props => {
+  return (
+    <div>
+      <SEO
+        title="Maternity coordinators help manage high risk conditions"
+        description="Blue Cross of Idaho’s Maternity Management program helps pregnant mothers with chronic health conditions have healthy, safe pregnancies and childbirth."
+      />
+      <Fade>
+        <div className="pp-hero">
+          <div className="pp-hero__share">
+            <div className="pp-hero__share-content">
+              <h4>Share</h4>
+              <Sharing
+                title="Maternity coordinators help manage high risk conditions"
+                url="https://www.bcbs.com/the-health-of-america/healthequity/maternity-coordinators-help-manage-high-risk-conditions/"
+              />
+            </div>
+          </div>
+          <div
+            className="pp-hero__image"
+            style={{
+              backgroundImage: `url("https://www.bcbs.com/sites/default/files/healthequity/images/brief-maternity-coordinators-help-manage-high-risk-conditions.jpg")`,
+            }}
+          >
+            <div className="pp-hero__meta">
+              <div className="pp-hero__meta-content">
+                <div className="pp-hero__category">
+                  <span>Maternal health</span>
+                </div>
+                <div className="pp-hero__title">
+                  <h1>
+                    Maternity coordinators help manage high risk conditions
+                  </h1>
+                </div>
+                <div className="pp-hero__author">
+                  <span>Kristin Gourlay</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Fade>
+
+      <div className="pp-main">
+        <Fade>
+          <div className="pp-related">
+            <h5 className="pp-related__title">Related</h5>
+            <ul className="pp-related__list">
+              <li className="pp-related__list-item">
+                <div className="pp-related__category">
+                  <span>Report</span>
+                </div>
+                <div className="pp-related__text">
+                  <a href="https://www.bcbs.com/the-health-of-america/reports/trends-in-pregnancy-and-childbirth-complications-in-the-us">
+                    Trends in Pregnancy and Childbirth Complications in the US
+                  </a>
+                </div>
+              </li>
+              <li className="pp-related__list-item">
+                <div className="pp-related__category">
+                  <span>Article</span>
+                </div>
+                <div className="pp-related__text">
+                  <a href="https://www.bcbs.com/the-health-of-america/articles/bcbs-companies-support-new-and-expectant-mothers">
+                    BCBS companies support new and expectant mothers
+                  </a>
+                </div>
+              </li>
+              <li className="pp-related__list-item">
+                <div className="pp-related__category">
+                  <span>Podcast</span>
+                </div>
+                <div className="pp-related__text">
+                  <a href="https://www.bcbs.com/the-health-of-america/podcast/podcast-improving-maternal-health-starts-pregnancy">
+                    Improving maternal health starts before pregnancy
+                  </a>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </Fade>
+        <Fade>
+          <div className="pp-content">
+            <div className="pp-content__inner">
+              <p>
+                Blue Cross of Idaho’s Maternity Management is a comprehensive
+                program to help pregnant women with acute or chronic health
+                conditions have a safe and healthy pregnancy and childbirth. A
+                Maternity Coordinator first conducts an initial risk assessment
+                with members. If risks are identified, members are connected
+                with a case manager who collaborates with mothers to develop a
+                personalized care plan. The case manager can help women identify
+                any barriers to achieving goals, such as reducing high blood
+                pressure, collaborate with a woman’s doctors and check in once
+                or more a trimester to provide guidance and answer questions.
+              </p>
+              <p>
+                <em>
+                  Blue Cross of Idaho is a licensee of the Blue Cross Blue
+                  Shield Association, an association of independent, locally
+                  owned Blue Cross and Blue Shield companies.
+                </em>
+              </p>
+            </div>
+          </div>
+        </Fade>
+      </div>
+
+      <Fade>
+        <div className="pp-explore">
+          <h2>Explore more stories</h2>
+          <InstantSearch searchClient={searchClient} indexName="test_index">
+            <Configure
+              hitsPerPage={2}
+              distinct
+              filters="headline:'Using data and technology to identify and support high-risk pregnancies' OR headline:'Improving postpartum care for high-risk mothers'"
+            />
+
+            <InfiniteHits hitComponent={planResult} />
+          </InstantSearch>
+        </div>
+      </Fade>
+    </div>
+  )
+}
+
+export default MaternityCoordinators
