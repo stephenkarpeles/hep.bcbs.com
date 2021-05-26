83f105bc846b7a4371d477adc0b4cb1c96e9d89d
John Cionci <john@thinkoomph.com>
2021-05-26 13:14:49 -0400

    add export functions to Algolia file, import in most other page files

---
+++ src/components/Algolia.js
@@ -0,0 +1,33 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import algoliasearch from "algoliasearch/lite"
+
+export const searchClient = algoliasearch(
+  "B604WWKJH0",
+  "d5f4c69eedaa62952d698d108856f2a0"
+)
+
+export const planResult = ({ hit }) => {
+  const { headline, topic, plans, slug, teaser, img_url, read_time } = hit
+
+  return (
+    <div className="plan-result-card">
+      <Link to={`/${slug}/`}></Link>
+      <div className="plan-result-card-plan">
+        <div className="plan-result-card-image">
+          <img
+            src={`https://www.bcbs.com/sites/default/files/healthequity/images/${img_url}`}
+            alt=""
+          />
+        </div>
+        <div className="plan-result-card-company">{plans}</div>
+      </div>
+      <div className="plan-result-card-meta">
+        <div className="plan-result-card-category">{topic}</div>
+        <div className="plan-result-card-title">{headline}</div>
+        <div className="plan-result-card-excerpt">{teaser}</div>
+        <div className="plan-result-card-read">{read_time} min read</div>
+      </div>
+    </div>
+  )
+}
