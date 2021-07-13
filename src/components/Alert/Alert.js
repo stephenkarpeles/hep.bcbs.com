2ebdf89426a08d8cf89a21cc8e663d7d953503a7
John Cionci <john@thinkoomph.com>
2021-07-13 18:34:48 -0400

    update styles and template for alert banner

--- src/components/Alert/Alert.js
+++ src/components/Alert/Alert.js
@@ -1,20 +1,57 @@
-import * as React from "react"
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 import { AnchorLink } from "gatsby-plugin-anchor-links"
 import "./Alert.css"
 
 const AlertBanner = () => {
+  const data = useStaticQuery(graphql`
+    {
+      allNodeHealthEquityEmergencyAlert {
+        edges {
+          node {
+            title
+            field_alert_color
+            field_icon
+            field_he_anchor_link
+            field_he_link {
+              title
+              uri
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  const alert = data.allNodeHealthEquityEmergencyAlert.edges[0].node
+
   return (
-    <section className="alert alert-warning" id="alert">
+    <section className={`alert alert-${alert.field_alert_color}`} id="alert">
       <div className="max-container">
         <div className="alert-text">
-          <AnchorLink
-            to="/#community"
-            className="navigation__anchor-link"
-            title="What we're doing in your community"
-          >
-            Read about how Blue Cross and Blue Shield companies are reimagining
-            a more equitable healthcare system
-          </AnchorLink>
+          {console.log(alert.field_he_link)}
+          {alert?.field_he_anchor_link ? (
+            <>
+              <AnchorLink
+                to={`${alert?.field_he_link?.uri}`}
+                className="navigation__anchor-link"
+                title={`${alert?.field_he_link?.title}`}
+              >
+                {alert.title}
+              </AnchorLink>
+            </>
+          ) : (
+            <>
+              <a
+                className="navigation__anchor-link"
+                title={`${alert?.field_he_link?.title}`}
+                href={alert?.field_he_link?.uri}
+              >
+                {alert.title}
+              </a>
+            </>
+          )}
+          <span className={`icon ${alert.field_icon}`}></span>
         </div>
       </div>
     </section>
