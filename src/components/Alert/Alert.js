dc2675edca3206232c0d6819370262f4a1dbd326
John Cionci <john@thinkoomph.com>
2021-07-14 16:52:25 -0400

    uodate output and conditinals for alerts, new markup for goals

--- src/components/Alert/Alert.js
+++ src/components/Alert/Alert.js
@@ -1,58 +1,103 @@
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
+import { Link, useStaticQuery, graphql } from "gatsby"
 import { AnchorLink } from "gatsby-plugin-anchor-links"
 import "./Alert.css"
 
 const AlertBanner = () => {
   const data = useStaticQuery(graphql`
     {
-      allNodeHealthEquityEmergencyAlert {
+      allNodeHealthEquityEmergencyAlert(
+        limit: 1
+        sort: { fields: changed, order: DESC }
+      ) {
         edges {
           node {
             title
             field_alert_color
             field_icon
-            field_he_anchor_link
-            field_he_link {
-              title
-              uri
+            field_he_anchor_link {
+              url
             }
+            field_he_link {
+              url
+            }
+            field_he_internal_link {
+              url
+            }
+            field_he_external_link {
+              url
+            }
+            field_he_link_type
           }
         }
+      }
+      site {
+        pathPrefix
       }
     }
   `)
 
   const alert = data.allNodeHealthEquityEmergencyAlert.edges[0].node
+  const linkType = alert.field_he_link_type
+
+  let linkDisplay
+  // an anchor link within the current page
+  if (linkType === "anchor") {
+    linkDisplay = (
+      <AnchorLink
+        to={`${alert?.field_he_anchor_link?.url}`}
+        className="navigation__anchor-link"
+        title={`${alert?.title}`}
+      >
+        {alert.title}
+        <span className={`icon ${alert.field_icon}`}></span>
+      </AnchorLink>
+    )
+  }
+
+  // an internal link within the health equity site
+  if (linkType === "healthequity") {
+    linkDisplay = (
+      <Link
+        to={`${alert?.field_he_link?.url}`}
+        className=""
+        title={`${alert?.title}`}
+      >
+        {alert.title}
+        <span className={`icon ${alert.field_icon}`}></span>
+      </Link>
+    )
+  }
+
+  // an internal link within the bcbs domain
+  if (linkType === "internal") {
+    linkDisplay = (
+      <a title={`${alert?.title}`} href={alert?.field_he_internal_link?.url}>
+        {alert.title}
+        <span className={`icon ${alert.field_icon}`}></span>
+      </a>
+    )
+  }
+
+  // an external link outside the bcbs domain
+  if (linkType === "external") {
+    linkDisplay = (
+      <a
+        target="_blank"
+        rel="noreferrer"
+        title={`${alert?.title}`}
+        href={alert?.field_he_external_link?.url}
+      >
+        {alert.title}
+        <span className={`icon ${alert.field_icon}`}></span>
+      </a>
+    )
+  }
 
   return (
     <section className={`alert alert-${alert.field_alert_color}`} id="alert">
       <div className="max-container">
-        <div className="alert-text">
-          {console.log(alert.field_he_link)}
-          {alert?.field_he_anchor_link ? (
-            <>
-              <AnchorLink
-                to={`${alert?.field_he_link?.uri}`}
-                className="navigation__anchor-link"
-                title={`${alert?.field_he_link?.title}`}
-              >
-                {alert.title}
-                <span className={`icon ${alert.field_icon}`}></span>
-              </AnchorLink>
-            </>
-          ) : (
-            <>
-              <a
-                title={`${alert?.field_he_link?.title}`}
-                href={alert?.field_he_link?.uri}
-              >
-                {alert.title}
-                <span className={`icon ${alert.field_icon}`}></span>
-              </a>
-            </>
-          )}
-        </div>
+        <div className="alert-text">{linkDisplay}</div>
       </div>
     </section>
   )
--- src/components/Goal.js
+++ src/components/Goal.js
@@ -25,28 +25,28 @@ const OurGoal = () => {
     }
   `)
 
+  const goal = data.allNodeHealthEquityGoals.edges[0].node
+
   return (
     <Slide right>
       <section className="goal-banner" id="commitment">
         <div className="max-container">
           <div className="goal-banner-box">
-            <h6 className="goal-banner-box-title">
-              {data.allNodeHealthEquityGoals.edges[0].node.title}
-            </h6>
+            <h6 className="goal-banner-box-title">{goal.title}</h6>
             <div className="goal-banner-box-description">
-              {
-                data.allNodeHealthEquityGoals.edges[0].node
-                  .field_he_statistic_prefix.value
-              }
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: goal.field_he_statistic_prefix.value,
+                }}
+              />
             </div>
             <div className="goal-banner-box-stat">
-              {data.allNodeHealthEquityGoals.edges[0].node.field_he_statistic}
+              <div
+                dangerouslySetInnerHTML={{ __html: goal.field_he_statistic }}
+              />
               <span>%</span>
               <span className="goal-banner-box-text">
-                {
-                  data.allNodeHealthEquityGoals.edges[0].node
-                    .field_he_statistic_suffix.value
-                }
+                {goal.field_he_statistic_suffix.value}
               </span>
             </div>
           </div>
