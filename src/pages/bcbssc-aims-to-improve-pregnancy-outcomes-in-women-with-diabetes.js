d81a2d19d2d525f17128805d83817c04b87c9091
John Cionci <john@thinkoomph.com>
2021-07-16 10:08:00 -0400

    add new video to content page

--- src/pages/bcbssc-aims-to-improve-pregnancy-outcomes-in-women-with-diabetes.js
+++ src/pages/bcbssc-aims-to-improve-pregnancy-outcomes-in-women-with-diabetes.js
@@ -99,6 +99,15 @@ const ImprovePregnancyOutcomes = props => {
         <Fade>
           <div className="pp-content">
             <div className="pp-content__inner">
+              <iframe
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/gUUk8ajU_lQ"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
               <p>
                 BlueCross BlueShield of South Carolina and the BlueCross
                 BlueShield of South Carolina Foundation launched Diabetes Free
