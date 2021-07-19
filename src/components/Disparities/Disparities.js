345c62422bcd182b40497493c5d6ab0bb741673a
John Cionci <john@thinkoomph.com>
2021-07-19 10:07:20 -0400

    update layout and styles

--- src/components/Disparities/Disparities.js
+++ src/components/Disparities/Disparities.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react"
 import { useStaticQuery, graphql } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import Fade from "react-reveal/Fade"
 import CountUp from "react-countup"
 import VisibilitySensor from "react-visibility-sensor"
+import "./Disparities.css"
 
-const Disparities = () => {
+const Disparities = ({ header }) => {
   const [viewPortEntered, setViewPortEntered] = useState(false)
   const data = useStaticQuery(graphql`
     {
@@ -13,17 +14,23 @@ const Disparities = () => {
         edges {
           node {
             title
+            body {
+              value
+            }
             field_he_statistic
             field_he_statistic_text
             field_he_dis_content_source {
               uri
             }
-            field_he_dis_featured_image {
-              title
-            }
+
             relationships {
               field_topics {
                 name
+              }
+              field_he_dis_featured_image {
+                uri {
+                  value
+                }
               }
             }
           }
@@ -32,109 +39,88 @@ const Disparities = () => {
     }
   `)
 
-  const disparities = data.allNodeHealthEquityDisparities.edges
+  const disparities = data.allNodeHealthEquityDisparities
+  // const image = getImage(disparities.edges.node.field_he_dis_featured_image)
+
   console.log(data)
 
   return (
     <section className="disparities" id="disparities">
       <div className="inner-content">
         <Fade>
-          <div className="disparities-heading">
-            Health Disparities: By the Numbers
-          </div>
+          <div className="disparities-heading">{header}</div>
         </Fade>
         <div className="disparities-cards-wrap">
           <Fade bottom cascade>
             <div className="disparities-cards">
-              {disparities.map(
-                disparity => disparity.node.relationships.field_topics.name
-              )}
-              <div
-                className={`disparities-card disparities-card-${disparities.node.relationships.field_topics[0].name}`}
-              >
-                <div className="disparities-card-heading">
-                  {disparities.relationships.field_topics[0].name}
+              {disparities.edges.map((disparity, idx) => (
+                <div key={idx} className="disparities-card disparities-card">
+                  {disparity.node.relationships.field_topics
+                    .slice(0, 1)
+                    .map((field_topics, idx) => (
+                      <div key={idx} className="disparities-card-heading">
+                        {field_topics.name}
+                      </div>
+                    ))}
+                  <div className="disparities-card-stat">
+                    <CountUp
+                      start={viewPortEntered ? null : 0}
+                      end={disparity.node.field_he_statistic}
+                      redraw={true}
+                      duration={0.5}
+                      useEasing={false}
+                    >
+                      {({ countUpRef }) => (
+                        <VisibilitySensor
+                          active={!viewPortEntered}
+                          onChange={isVisible => {
+                            if (isVisible) {
+                              setViewPortEntered(true)
+                            }
+                          }}
+                          delayedCall
+                        >
+                          <span ref={countUpRef} />
+                        </VisibilitySensor>
+                      )}
+                    </CountUp>
+                    <span>{disparity.node.field_he_statistic_text}</span>
+                  </div>
+                  <div
+                    className="disparities-card-text"
+                    dangerouslySetInnerHTML={{
+                      __html: disparity.node.body.value,
+                    }}
+                  ></div>
+                  <div className="disparities-card-image">
+                    {/* <StaticImage
+                      src={
+                        disparity.node.relationships.field_he_dis_featured_image
+                          .uri.value
+                      }
+                      alt=""
+                    /> */}
+                    {/* <GatsbyImage image={image} alt="" /> */}
+                  </div>
                 </div>
-                <div className="disparities-card-stat">
-                  <CountUp
-                    start={viewPortEntered ? null : 0}
-                    end={70}
-                    redraw={true}
-                    duration={0.5}
-                    useEasing={false}
-                  >
-                    {({ countUpRef }) => (
-                      <VisibilitySensor
-                        active={!viewPortEntered}
-                        onChange={isVisible => {
-                          if (isVisible) {
-                            setViewPortEntered(true)
-                          }
-                        }}
-                        delayedCall
-                      >
-                        <span ref={countUpRef} />
-                      </VisibilitySensor>
-                    )}
-                  </CountUp>
-                  <span>%</span>
-                </div>
-                <div className="disparities-card-text">
-                  Black men are 70% more likely to die from a stroke as compared
-                  to non-Hispanic white men.
-                </div>
-                <div className="disparities-card-image">
-                  <StaticImage
-                    src="../images/illustrations/illustration-cardiovascular-health.png"
-                    alt=""
-                  />
-                </div>
-              </div>
+              ))}
             </div>
           </Fade>
         </div>
         <div className="sources">
-          Sources: 1){" "}
-          <u>
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href="https://minorityhealth.hhs.gov/omh/browse.aspx?lvl=4&lvlid=28"
-            >
-              OMH: Stroke and African Americans
-            </a>
-          </u>{" "}
-          2){" "}
-          <u>
-            <a
-              href="https://www.bcbs.com/the-health-of-america/reports/millennial-health-trends-behavioral-health-conditions"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Millennial Health: Trends in Behavioral Health Conditions
-            </a>
-          </u>{" "}
-          3){" "}
-          <u>
-            <a
-              href="https://www.cdc.gov/diabetes/pdfs/data/statistics/national-diabetes-statistics-report.pdf"
-              target="_blank"
-              rel="noreferrer"
-            >
-              CDC: National Diabetes Statistics Report
-            </a>
-          </u>{" "}
-          4){" "}
-          <u>
-            <a
-              href="https://www.ajmc.com/view/racial-disparities-persist-in-maternal-morbidity-mortality-and-infant-health"
-              target="_blank"
-              rel="noreferrer"
-            >
-              AJMC: Racial Disparities Persist in Maternal Morbidity, Mortality
-              and Infant Health
-            </a>
-          </u>
+          <span>Sources:</span>{" "}
+          {disparities.edges.map((source, idx) => (
+            <span key={idx}>
+              {idx + 1}){" "}
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href={source.node.field_he_dis_content_source.uri}
+              >
+                {source.node.title}
+              </a>{" "}
+            </span>
+          ))}
         </div>
       </div>
     </section>
