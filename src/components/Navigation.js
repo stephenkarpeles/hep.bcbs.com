8f06d6031f68add9e441b0bb1e6f953c13bf0e3c
John Cionci <john@thinkoomph.com>
2021-08-25 10:07:06 -0400

    update desktop navigation styles

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,65 +3,65 @@ import { AnchorLink } from "gatsby-plugin-anchor-links"
 import { Link } from "gatsby"
 
 function Navigation() {
-  
   const menuOpenClass = "mobile-menu-is-open"
 
   function removeMenuOpenClass() {
-    document.body.classList.remove(menuOpenClass);
+    document.body.classList.remove(menuOpenClass)
   }
 
   function hideMobileNav() {
-    document.querySelector('header').classList.remove('navigation-mobile');
-    document.querySelector('header').classList.add('navigation');
+    document.querySelector("header").classList.remove("navigation-mobile")
+    document.querySelector("header").classList.add("navigation")
   }
 
   return (
-    <div className="navigation">
+    <div className="navigation navigation-main">
       <div className="max-container">
         <nav>
-          <Link 
-            className="he-landing" 
-            to="/">
+          <Link className="he-landing" to="/">
             Health Equity
           </Link>
-          <AnchorLink 
+          <AnchorLink
             onAnchorLinkClick={() => {
-              removeMenuOpenClass();
-              hideMobileNav();
+              removeMenuOpenClass()
+              hideMobileNav()
             }}
-            onKeyDown={removeMenuOpenClass} 
-            to="/#healthequity" 
+            onKeyDown={removeMenuOpenClass}
+            to="/#healthequity"
             className="navigation__anchor-link"
-            title="What is Health Equity">
+            title="What is Health Equity"
+          >
             <span>National Health Equity Strategy</span>
           </AnchorLink>
-          <AnchorLink 
+          <AnchorLink
             onAnchorLinkClick={() => {
-              removeMenuOpenClass();
-              hideMobileNav();
+              removeMenuOpenClass()
+              hideMobileNav()
             }}
-            onKeyDown={removeMenuOpenClass} 
-            to="/#disparities" 
+            onKeyDown={removeMenuOpenClass}
+            to="/#disparities"
             className="navigation__anchor-link"
-            title="Health Disparities">
+            title="Health Disparities"
+          >
             <span>Health Disparities: By the Numbers</span>
           </AnchorLink>
-          <AnchorLink 
+          <AnchorLink
             onAnchorLinkClick={() => {
-              removeMenuOpenClass();
-              hideMobileNav();
-            }} 
-            onKeyDown={removeMenuOpenClass} 
-            to="/#commitment" 
+              removeMenuOpenClass()
+              hideMobileNav()
+            }}
+            onKeyDown={removeMenuOpenClass}
+            to="/#commitment"
             className="navigation__anchor-link"
-            title="Our Commitment">
+            title="Our Commitment"
+          >
             <span>Our Goal</span>
           </AnchorLink>
-          <AnchorLink 
+          <AnchorLink
             onAnchorLinkClick={() => {
-              removeMenuOpenClass();
-              hideMobileNav();
-            }} 
+              removeMenuOpenClass()
+              hideMobileNav()
+            }}
             onKeyDown={removeMenuOpenClass}
             to="/#community"
             className="navigation__anchor-link"
@@ -69,29 +69,31 @@ function Navigation() {
           >
             <span>What We're Doing in Your Community</span>
           </AnchorLink>
-          <AnchorLink 
+          <AnchorLink
             onAnchorLinkClick={() => {
-              removeMenuOpenClass();
-              hideMobileNav();
-            }} 
-            onKeyDown={removeMenuOpenClass} 
-            to="/#advisory" 
+              removeMenuOpenClass()
+              hideMobileNav()
+            }}
+            onKeyDown={removeMenuOpenClass}
+            to="/#advisory"
             className="navigation__anchor-link"
-            title="Meet our Advisory Panel">
+            title="Meet our Advisory Panel"
+          >
             <span>Meet Our Advisory Panel</span>
           </AnchorLink>
           {/* <AnchorLink to="/#news" title="News">
             <span>News</span>
           </AnchorLink> */}
-          <AnchorLink 
+          <AnchorLink
             onAnchorLinkClick={() => {
-              removeMenuOpenClass();
-              hideMobileNav();
-            }} 
+              removeMenuOpenClass()
+              hideMobileNav()
+            }}
             onKeyDown={removeMenuOpenClass}
-            to="/press-kit" 
+            to="/press-kit"
             className="navigation__anchor-link"
-            title="Press Kit">
+            title="Press Kit"
+          >
             <span>Press Kit</span>
           </AnchorLink>
         </nav>
