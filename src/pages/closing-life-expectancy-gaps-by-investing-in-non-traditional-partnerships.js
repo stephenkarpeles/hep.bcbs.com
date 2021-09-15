4357067881defb8b33b370434f6ce2f2ee520a38
John Cionci <john@thinkoomph.com>
2021-09-15 09:31:02 -0400

    add video embed, update explore more styles

--- src/pages/closing-life-expectancy-gaps-by-investing-in-non-traditional-partnerships.js
+++ src/pages/closing-life-expectancy-gaps-by-investing-in-non-traditional-partnerships.js
@@ -114,6 +114,15 @@ const ClosingLifeExpectancy = props => {
                   barriers to good health.
                 </em>
               </p>
+              <iframe
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/JvEwQy66kr0"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
               <p>
                 The Chicago communities of Englewood and Streeterville are only
                 10 miles apart. Despite that proximity, the difference in life
