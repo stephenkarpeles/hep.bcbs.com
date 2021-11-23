0fdb6c15e6319414d51436df518a436053d7e39b
John Cionci <john@thinkoomph.com>
2021-11-23 11:24:34 -0500

    add GO script to build

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,4 +4,30 @@
  * See: https://www.gatsbyjs.com/docs/ssr-apis/
  */
 
-// You can delete this file if you're not using it
+import * as React from "react"
+
+/**
+ * Add the Google Optimize Script to Health Equity
+ * @see https://medium.com/@pisanocaroline/optimizing-google-optimize-in-gatsby-js-e67d0a59bfa1
+ */
+exports.onRenderBody = ({ setHeadComponents }) => {
+  //if (process.env.DEPLOY_ENV === `prod`) {
+  // Add Google Optimize Script
+  setHeadComponents([
+    <script
+      async
+      src="https://www.googleoptimize.com/optimize.js?id=GTM-NM59FZ9"
+    ></script>,
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `
+         window.dataLayer = window.dataLayer || [];
+         function gtag(){dataLayer.push(arguments);}
+         gtag('js', new Date());
+         gtag('config', 'UA-3312038-1', { 'optimize_id': 'GTM-NM59FZ9'});
+         `,
+      }}
+    />,
+  ])
+  //}
+}
