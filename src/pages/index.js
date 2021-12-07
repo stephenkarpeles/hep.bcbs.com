030a9d2bd864a6e50fb74996c227e21194411b63
John Cionci <john@thinkoomph.com>
2021-12-07 10:39:08 -0500

    update index for new content

--- src/pages/index.js
+++ src/pages/index.js
@@ -188,7 +188,7 @@ const IndexPage = props => {
               hitsPerPage={6}
               clickAnalytics
               distinct
-              filters="topic:'Maternal Health' AND (NOT headline:'Congress can help save pregnant women and mothers of color' AND NOT headline:'Reporting racial disparities in healthcare quality spurs accountability')"
+              filters="topic:'Maternal Health' AND (NOT headline:'Moving from Awareness to Action for a Healthier Beginning' AND NOT headline:'Congress can help save pregnant women and mothers of color')"
             />
 
             <Hits hitComponent={planResult} />
