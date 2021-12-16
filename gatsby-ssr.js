64d0a7ad2ee5067f25d754a1509033ffc1da95a4
John Cionci <john@thinkoomph.com>
2021-12-16 16:12:27 -0500

    remove optimize script

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,31 +3,3 @@
  *
  * See: https://www.gatsbyjs.com/docs/ssr-apis/
  */
-
-const React = require("react")
-
-/**
- * Add the Google Optimize Script to Health Equity
- * @see https://medium.com/@pisanocaroline/optimizing-google-optimize-in-gatsby-js-e67d0a59bfa1
- */
-exports.onRenderBody = ({ setHeadComponents }) => {
-  if (process.env.DEPLOY_ENV === `prod`) {
-    // Add Google Optimize Script
-    setHeadComponents([
-      <script
-        async
-        src="https://www.googleoptimize.com/optimize.js?id=GTM-NM59FZ9"
-      ></script>,
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-         window.dataLayer = window.dataLayer || [];
-         function gtag(){dataLayer.push(arguments);}
-         gtag('js', new Date());
-         gtag('config', 'UA-3312038-1', { 'optimize_id': 'GTM-NM59FZ9'});
-         `,
-        }}
-      />,
-    ])
-  }
-}
