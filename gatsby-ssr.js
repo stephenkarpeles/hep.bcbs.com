7e773cac5c0abdd66639813d3138cabfcbaa6407
John Cionci <john@thinkoomph.com>
2022-01-11 13:35:25 -0500

    update MP langcode

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -15,36 +15,10 @@ exports.onRenderBody = ({ setHeadComponents, setPostBodyComponents }) => {
       src="mp_linkcode.js"
     ></script>,
     <script
-      key="motionpoint-script-data"
-      dangerouslySetInnerHTML={{
-        __html: `
-        MP.UrlLang='mp_js_current_lang';
-        MP.SrcUrl=decodeURIComponent('mp_js_orgin_url');
-        MP.oSite=decodeURIComponent('mp_js_origin_baseUrl');
-        MP.tSite=decodeURIComponent('mp_js_translated_baseUrl');
-        MP.init();
-        var mp_langLink = function() {
-	        var langlinks = document.querySelectorAll('.langLink');
-	        for (var i = 0; i < langlinks.length; i++) {
-		        langlinks.item(i).setAttribute('href', '');
-		        langlinks.item(i).onclick = function() {
-			        MP.init();
-			        var lang = this.getAttribute('data-lang');
-			        var url = this.getAttribute('data-href');
-			        var tSite = MP.tSite.replace(/(https?:\/\/|\/?$)/g,'');
-			        url = url.replace(/(https?:\/\/|\/?$)/g,'');
-			        MP.switchLanguage(tSite.search(url)!=-1?MP.oSite:url, lang, true);
-			        return false;
-		        }
-	        }
-        };
-        if(window.addEventListener){
-	        window.addEventListener('load',mp_langLink,false);
-        }else if(window.attachEvent){
-	        window.attachEvent('onload',mp_langLink);
-        }`,
-      }}
-    />,
+      type="text/javascript"
+      key="motionpoint-script-lang"
+      src="mp_langcode.js"
+    ></script>,
   ]),
     setPostBodyComponents([
       // MotionPoint closing script
